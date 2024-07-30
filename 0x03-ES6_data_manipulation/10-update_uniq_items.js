2b9c0407846e3fb36456e4390defd93f59ccc163
Matumbai Binale <binalematumbai@gmail.com>
2024-07-30 05:38:14 +0300

    (task 10): Check if argument is a Map object

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,9 @@
 // returns an updated map for all items with initial quantity at 1
 export default function updateUniqueItems(itemsMap) {
+  if (!(itemsMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
   for (const [k, v] of itemsMap.entries()) {
     if (v === 1) {
       itemsMap.set(k, 100);
